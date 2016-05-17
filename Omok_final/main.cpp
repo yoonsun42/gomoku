e70c3b7732d45b4a998200d3a81f83c66e99e628
yoonsun42 <smartys@korea.ac.kr>
2016-05-17 17:05:52 +0900

    Initial Commit

---
+++ Omok_final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Omok_final
+//
+//  Created by 정윤선 on 2016. 5. 17..
+//  Copyright (c) 2016년 정윤선. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
